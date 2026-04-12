11ef8e84a38289731fe54d6df9fe36c201fa0433
Bryan Romero <bromerosite24@gmail.com>
2026-04-12 14:14:12 -0400

    fix import Alert

--- core/views.py
+++ core/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import ChildProfileForm
-from .models import ChildProfile, GameSession, Metrics, alert
+from .models import ChildProfile, GameSession, Metrics, Alert
 from django.shortcuts import get_object_or_404
 from django.http import JsonResponse
 import json
